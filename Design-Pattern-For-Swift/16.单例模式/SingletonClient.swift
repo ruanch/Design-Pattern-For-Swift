cec0916592d93240599e400255cec741748a7fe0
阮沧晖 <rch@xl-ebike.com>
2018-01-16 12:14:07 +0800

    单例模式示例

---
+++ Design-Pattern-For-Swift/16.单例模式/SingletonClient.swift
@@ -0,0 +1,39 @@
+//
+//  SingletonClient.swift
+//  Design-Pattern-For-Swift
+//
+//  Created by 阮沧晖 on 2018/1/16.
+//  Copyright © 2018年 阮沧晖. All rights reserved.
+//
+
+import UIKit
+
+class SingletonClient: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        //单例：游戏设置界面一次设置，工程通用
+        let gameSetting = NEGameSetting .sharedInstance1
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Design-Pattern-For-Swift/16.单例模式/单例/NEGameSetting.swift
@@ -0,0 +1,39 @@
+//
+//  NEGameSetting.swift
+//  Design-Pattern-For-Swift
+//
+//  Created by 阮沧晖 on 2018/1/16.
+//  Copyright © 2018年 阮沧晖. All rights reserved.
+//
+
+import UIKit
+
+let _SingletonSharedInstance = NEGameSetting()
+
+class NEGameSetting: NSObject {
+
+    //第一种写法
+    class var sharedInstance : NEGameSetting {
+        struct Static{
+            static var onceToken: dispatch_once_t = 0
+            static var instance: NEGameSetting? = nil
+        }
+        dispatch_once(&Static.onceToken){
+            Static.instance = NEGameSetting()
+        }
+        return Static.instance!
+    }
+    
+    //第二种写法
+    class var  sharedInstance1 : NEGameSetting{
+        struct Static{
+            static var instance: NEGameSetting = NEGameSetting()
+        }
+        return Static.instance
+    }
+    
+    //第三种写法
+    class var sharedInstance2 : NEGameSetting{
+        return _SingletonSharedInstance
+    }
+}
